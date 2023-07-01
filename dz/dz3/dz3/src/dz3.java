2dc089038d73480f7f2e6d24c4733e50f95bca86
Alexey.Butenko <alexeyb7@gmail.com>
2023-07-01 21:58:01 +0300

    Modified dz3

--- dz/dz3/dz3/src/dz3.java
+++ dz/dz3/dz3/src/dz3.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class dz3 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) {}
+        public List<Integer> intsInRange(int size, int lowerBound, int upperBound) 
+        { Random random = new Random();
+             List<Integer> result = new ArrayList<>();
+            for (int i = 0; i < size; i++)
+            { result.add(random.nextInt(upperBound - lowerBound) + lowerBound);
+             } 
+        return result; }
+
+        List<Integer> integerList = intsInRange(5, 0, 10);
+
+        //System.out.@printf(integerList(0));       
+
 }
