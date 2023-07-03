bdfa59a9622f42b89ac3f1421b5691870b5fa36f
Alexey.Butenko <alexeyb7@gmail.com>
2023-07-03 23:22:32 +0300

    Add dz2_2

---
+++ dz/dz2/dz2_2/src/dz2_2.java
@@ -0,0 +1,65 @@
+//// 2) Реализуйте алгоритм сортировки пузырьком числового
+//// массива, результат после каждой итерации запишите в лог-файл.*/
+
+import java.io.*;
+
+    class ArrayBubble{
+        private int[] a;   //ссылка на массив
+        private int elems;  //количество элементов в массиве
+    
+        public ArrayBubble(int max){    //конструктор класса
+            a = new int[max];          //создание массива размером max
+            elems = 0;                  //при создании массив содержит 0 элементов
+        }
+    
+        public void into(int value){   //метод вставки элемента в массив
+            a[elems] = value;           //вставка value в массив a
+            elems++;                    //размер массива увеличивается
+        }
+    
+        public void printer(){          //метод вывода массива в консоль
+           // OutputStream output = new FileOutputStream("./file.txt"); // Создание текстового файла
+            for (int i = 0; i < elems; i++){    //для каждого элемента в массиве
+                System.out.print(a[i] + " ");   //вывести в консоль
+                //output.write((a[i])); // Запись каждого элемента в текстовый файл
+                System.out.println("");         //с новой строки
+            }
+            System.out.println("----Окончание вывода массива----");
+            //output.close();
+        }
+    
+        private void toSwap(int first, int second){ //метод меняет местами пару чисел массива
+            int dummy = a[first];      //во временную переменную помещаем первый элемент
+            a[first] = a[second];       //на место первого ставим второй элемент
+            a[second] = dummy;          //вместо второго элемента пишем первый из временной памяти
+        }
+    
+        public void bubbleSorter(){     //МЕТОД ПУЗЫРЬКОВОЙ СОРТИРОВКИ
+            for (int out = elems - 1; out >= 1; out--){  //Внешний цикл
+                for (int in = 0; in < out; in++){       //Внутренний цикл
+                    if(a[in] > a[in + 1])               //Если порядок элементов нарушен
+                        toSwap(in, in + 1);             //вызвать метод, меняющий местами
+                }
+            }
+        }
+    }
+    
+    
+
+
+    
+public class dz2_2 {
+    public static void main(String[] args) {
+        ArrayBubble array = new ArrayBubble(5); //Создаем массив array на 5 элементов
+
+        array.into(163);       //заполняем массив
+        array.into(300);
+        array.into(184);
+        array.into(191);
+        array.into(174);
+
+        array.printer();            //выводим элементы до сортировки
+        array.bubbleSorter();       //ИСПОЛЬЗУЕМ ПУЗЫРЬКОВУЮ СОРТИРОВКУ
+        array.printer();            //снова выводим отсортированный йсписок
+    }
+}
