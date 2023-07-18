3d1aeba8eebfab7288d1c6f39b284b0d7c632863
alexeyb7 <alexeyb7@gmail.com>
2023-07-18 16:08:19 +0300

    Add dz4_1 and  dz4_2.java

---
+++ dz/dz4/dz4_2.java
@@ -0,0 +1,40 @@
+//Даны два Deque представляющие два неотрицательных целых числа. Цифры хранятся в обратном порядке,
+// и каждый из их узлов содержит одну цифру.
+// Сложите два числа и верните сумму в виде связанного списка.
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+public class dz4_2 {
+
+    public static void main(String[] args) {
+        Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(1, 2, 3));
+        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(5, 4, 7));
+        // result [6,6,0,1]
+        dz4_2 task = new dz4_2();
+        Deque<Integer> result = task.sum(d1, d2);
+        System.out.println(result);
+    }
+
+    public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2) {
+        Deque<Integer> result = new ArrayDeque<>();
+        int carry = 0;
+        while (!d1.isEmpty() || !d2.isEmpty()) {
+            int sum = carry;
+            if (!d1.isEmpty()) {
+                sum += d1.removeFirst();
+            }
+            if (!d2.isEmpty()) {
+                sum += d2.removeFirst();
+            }
+            result.addLast(sum % 10);
+            carry = sum / 10;
+        }
+        if (carry != 0) {
+            result.addLast(carry);
+        }
+        return result;
+    }
+
+}
